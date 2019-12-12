edee10a23d006bdf8823e310024bc7b446a0befb
DavidConway <dconway099@gmail.com>
2019-12-12 14:57:00 +0000

    epress app api update working

--- WebDev2SPA/api/data/index.js
+++ WebDev2SPA/api/data/index.js
@@ -15,4 +15,20 @@ router.post('/', (req, res) => {
         res.status(400).send({ message: "Unable to find option in request. No Contact Found in body" });
     }
 });
+
+router.put('/:id', (req, res) => {
+    const key = req.params.id;
+    const updateitem = req.body;
+    const index = data.map((item) => {
+        return item.itemName;
+    }).indexOf(key);
+    if (index !== -1) {
+        data.splice(index, 1, {
+            name: updateitem.imageUrl, itemName: updateitem.itemName, options: updateitem.options
+        });
+        res.status(200).send({ message: 'item Updated' });
+    } else {
+        res.status(400).send({ message: 'Unable to find item in request. No item Found in body' });
+    }
+});
 export default router;
