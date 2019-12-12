51a7510ca88cd41f69a0ec246a138c17f464c0b5
DavidConway <dconway099@gmail.com>
2019-12-12 14:30:48 +0000

    epress app api push working

--- WebDev2SPA/api/data/index.js
+++ WebDev2SPA/api/data/index.js
@@ -6,4 +6,13 @@ router.get('/', (req, res) => {
     res.send({ data: data });
 });
 
+router.post('/', (req, res) => {
+    let newitem = req.body;
+    if (newitem) {
+        data.push({ imageUrl: newitem.imageUrl, itemName: newitem.itemName, options: newitem.options });
+        res.status(201).send({ message: "item added Created" });
+    } else {
+        res.status(400).send({ message: "Unable to find option in request. No Contact Found in body" });
+    }
+});
 export default router;
--- WebDev2SPA/index.js
+++ WebDev2SPA/index.js
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import dataRouter from './api/data';
-
+import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.use(express.static('public'));
+
+
 
 app.use('/api/data', dataRouter);
 app.use(express.static('public'));
